1164515d35891f7c316f846d389e578a53abb082
William Morgan <william.morgan@hotmail.co.uk>
2021-07-31 16:49:35 +0100

    Create clojure.init.el

---
+++ clojure.init.el
@@ -0,0 +1,50 @@
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; add clojure packages
+(defvar my-packages '(better-defaults 
+                      clojure-mode 
+                      cider
+                      projectile
+                      company
+                      smartparens
+                      rainbow-delimiters
+                      aggressive-indent))
+
+(dolist (p my-packages) (unless (package-installed-p p) (package-install p)))
+
+;; projectile keybinding
+(require 'projectile)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
+;; use Shift-arrow keys to move between windows
+(windmove-default-keybindings)
+
+;; smooth keyboard scrolling
+(setq scroll-step 1) 
+(setq column-number-mode t)
+
+;; set font
+(set-frame-font "Inconsolata-16")
+
+;; clohure editing tweaks
+(add-hook 'clojure-mode-hook #'smartparens-strict-mode)
+(define-key smartparens-mode-map (kbd "M-(") 'sp-wrap-round)
+(define-key smartparens-mode-map (kbd "M-[") 'sp-wrap-square)
+(define-key smartparens-mode-map (kbd "M-{") 'sp-wrap-curly)
+(define-key smartparens-mode-map (kbd "M-<delete>") 'sp-unwrap-sexp)
+(define-key smartparens-mode-map (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
+
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'aggressive-indent-mode)
+
+;; auto-complete
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
+;; don't display repl
+(setq cider-repl-pop-to-buffer-on-connect nil)
